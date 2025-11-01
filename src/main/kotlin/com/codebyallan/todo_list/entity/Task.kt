414571133e6a4b127da40c3a1d3a74d63f77dd8f
codebyallan <allan.pereira.dev@proton.me>
2025-10-31 21:54:30 -0300

    fix(entity): Add missing @Table annotation to Task entity

--- src/main/kotlin/com/codebyallan/todo_list/entity/Task.kt
+++ src/main/kotlin/com/codebyallan/todo_list/entity/Task.kt
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.Table
 import org.hibernate.annotations.CreationTimestamp
 import org.hibernate.annotations.UpdateTimestamp
 import java.time.Instant
@@ -14,6 +15,7 @@ import java.time.Instant
  *  This class contains all the data for a single task.
  */
 @Entity
+@Table(name = "tasks")
 data class Task(
     /**
      * The unique identifier of the table is of type long.
